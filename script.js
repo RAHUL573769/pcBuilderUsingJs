3c64ab2edc6c6b7c010e0343be70e49cd3257de6
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-15 10:57:20 +0600

    fwfafwa commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-alert("hello world")
+document.getElementById('sixteenGB').addEventListener('click',function(){
+    
+
+    updatePrice('memory-cost',500);
+
+})
+document.getElementById('eightGB').addEventListener('click',function(){
+    
+
+    updatePrice('memory-cost',200);
+
+})
+
+
+function updatePrice(itemId,price){
+
+    
+        const memoryCost=document.getElementById(itemId);
+        memoryCost.innerText=price;
+    
+        const bestPrice=document.getElementById("best-price").innerText;
+        const memoryPrice=document.getElementById("memory-cost").innerText;
+        const storagePrice=document.getElementById("storage-cost").innerText;
+        const deliveryPrice=document.getElementById("delivery-cost").innerText;
+    
+        const totalPrice=parseFloat(bestPrice)+parseFloat(memoryPrice)+parseFloat(storagePrice)+parseFloat(deliveryPrice);
+        const total=document.getElementById('total-price');
+        total.innerText=totalPrice;
+    
+    
+
+
+}
